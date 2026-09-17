cc71a3c02389ee5abeb39cf1c0f977cfab17a819
Annapurnaseed <gulsaba835@gmail.com>
2026-09-17 09:54:38 +0530

    Serve static files from the current directory

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+const path = require('path');
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'login.html'));
+});
+
 
 // Database Connection Pool (Supabase PostgreSQL)
 const pool = new Pool({
