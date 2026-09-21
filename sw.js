0355667e0bd7df81d96a31b8948613d352204cac
Annapurnaseed <gulsaba835@gmail.com>
2026-09-21 11:47:34 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
+// sw.js - Basic Service Worker
+self.addEventListener('install', (e) => {
+    console.log('Service Worker Installed');
+});
+self.addEventListener('fetch', (e) => {
+    // Fetch handler
+});
 
